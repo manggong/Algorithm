0b68a8f3384bc012b2f7d32f697bc6b905ad309f
manggong <jihwank1995@gmail.com>
2020-06-27 17:40:41 +0900

    Add auto checker

---
+++ SWEA/test.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+problem_number = sys.argv[-1]
+
+if len(sys.argv) == 1:
+    print("문제 번호를 넣어주세요")
+    sys.exit(1)
+
+os.system(f'python {problem_number}.py > {problem_number}_result.txt')
+result = os.system(f'cmp {problem_number}_result.txt {problem_number}_output.txt')
+
+
+if not result:
+    print("정답 입니다.")
+else:
+    print("오답 입니다.")
+
+os.system(f'rm -rf {problem_number}_result.txt')
