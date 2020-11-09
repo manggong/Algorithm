b6e7f93572fcc149d233eb6a3f41dbdf2c3eb9a9
manggong <jihwank1995@gmail.com>
2020-11-09 20:39:43 +0900

    ssort

---
+++ Algorithm_with_python/10_ssort.py
@@ -0,0 +1,19 @@
+def find_min_idx(a):
+    n = len(a)
+    min_idx = 0
+    for i in range(1,n):
+        if a[i] < a[min_idx]:
+            min_idx = i
+    return min_idx
+
+def sel_sort(a):
+    result = []
+    while a:
+        min_idx = find_min_idx(a)
+        value = a.pop(min_idx)
+        result.append(value)
+    return result
+
+arr = [25,26,1,23,4]
+
+print(sel_sort(arr))
