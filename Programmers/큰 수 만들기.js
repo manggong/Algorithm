064d5dcf9798e046ca47a8d6405c77442f2bd58b
manggong <jihwank1995@gmail.com>
2020-12-14 23:00:21 +0900

    큰 수 만들기 with javascript

---
+++ Programmers/큰 수 만들기.js
@@ -0,0 +1,23 @@
+function solution(number, k) {
+    let stack = []
+    for (let i=0; i < number.length; i++) { 
+
+        let now = number[i]
+
+        while (k > 0 && stack[stack.length - 1] < now) {
+            stack.pop()
+            k--
+        }
+        stack.push(now)
+    }
+    stack.splice(stack.length - k, k)
+
+    let answer = stack.join('')
+    return answer
+    
+}
+
+let number = "1924"
+let k = 2
+
+solution(number, k)
