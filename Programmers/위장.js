5ddba8b0c077056c23c8a21b22173bb8cf0dddc5
manggong <jihwank1995@gmail.com>
2020-12-16 23:20:56 +0900

    위장 with javascript

---
+++ Programmers/위장.js
@@ -0,0 +1,18 @@
+function solution(clothes) {
+    let answer = 1
+    let obj={}
+    for(let i= 0; i<clothes.length; i++){
+        obj[clothes[i][1]]=(obj[clothes[i][1]] || 1) + 1
+    }
+    for(let key in obj){
+        answer *= obj[key]
+    }
+    console.log(answer-1)
+    return answer-1
+}
+
+const clothes = [['yellow_hat', 'headgear'],
+                ['blue_sunglasses', 'eyewear'],
+                ['green_turban', 'headgear']]
+
+solution(clothes)
