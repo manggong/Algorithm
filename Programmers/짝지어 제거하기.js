736b11021c59142af24d3c6ac31a43ec7fa8e65e
manggong <jihwank1995@gmail.com>
2021-01-19 21:33:26 +0900

    짝지어 제거하기.js

---
+++ Programmers/짝지어 제거하기.js
@@ -0,0 +1,14 @@
+function solution(s){
+    var answer = 0
+    var temp = []
+    for(var i=0; i<s.length; i++){
+        if(temp.length == 0 || temp[temp.length-1] !== s[i]){
+        temp.push(s[i])
+        } else if(temp[temp.length-1] == s[i]){
+        temp.pop()
+        }
+    }
+    if(temp.length == 0) answer = 1
+    
+    return answer
+    }
