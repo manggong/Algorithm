99de5737349286e3e5d5ba2fb855e03f6bdb7401
manggong <jihwank1995@gmail.com>
2021-05-03 22:28:15 +0900

    Infinity One

---
+++ codeup/infinity_one.py
@@ -0,0 +1,16 @@
+n, k = map(int, input().split())
+
+answer = 0
+
+while n >= k:
+    while n % k !=0:
+        n -= 1
+        answer += 1
+    n //= k
+    answer += 1
+
+while n > 1:
+    n -= 1
+    answer += 1
+
+print(answer)
