0fa9f86dfd59559298d67382a1618ce39f496319
manggong <jihwank1995@gmail.com>
2021-05-03 21:55:07 +0900

    Small Number

---
+++ codeup/small_number.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+
+answer = 0
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    min_value = min(data)
+    answer = max(answer, min_value)
+
+print(answer)
