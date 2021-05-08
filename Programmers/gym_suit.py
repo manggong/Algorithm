5428e5d1bffc1581f7b77cc256eaec8042db9934
manggong <jihwank1995@gmail.com>
2021-05-08 18:32:35 +0900

    Gym Suit

---
+++ Programmers/gym_suit.py
@@ -0,0 +1,10 @@
+def solution(n, lost, reserve):
+    # set_reserve = set(reserve) - set(lost)
+    # set_lost = set(lost) - set(reserve)
+    
+    for i in reserve:
+        if i-1 in lost:
+            lost.remove(i-1)
+        elif i+1 in lost:
+            lost.remove(i+1)
+    return n - len(lost)
